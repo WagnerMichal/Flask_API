ec6d2229c1a66771a13da530f55c269a697c3766
WagnerMichal <misawagner10@seznam.cz>
2023-06-14 11:51:00 +0200

    Added get endpoints

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify, Response
+from controllers.movies import *
 from db import create_tables
 
 app = Flask(__name__)
 
 @app.route('/movies', methods=['GET', 'POST'])
 def create_movie():
-    pass
+    if request.method == 'GET':
+        return jsonify(get_movies())
 
 @app.route('/movies/<int:id>',  methods=['GET', 'PUT'])
 def update_movie(id):
-    pass
+    if request.method == 'GET':
+        movie = get_by_id(id)
+        if not movie:
+            return Response("id not found", status=404)
+        return jsonify(movie)
 
 if __name__ == '__main__':
     create_tables()
